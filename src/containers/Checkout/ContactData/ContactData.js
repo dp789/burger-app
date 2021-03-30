81fbfc5dd4ea244502ed2889e149a75c41310c5c
Deepak singh <dprocks.bbk@gmail.com>
2021-03-30 23:06:40 +0530

    new commit

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -48,8 +48,8 @@ class ContactData extends Component {
                 value: '',
                 validation: {
                     required: true,
-                    minLength: 5,
-                    maxLength: 5,
+                    minLength: 6,
+                    maxLength: 6,
                     isNumeric: true
                 },
                 valid: false,
